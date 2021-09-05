770ea7226d103602af5f9698932ba80472e7c5cf
Malindu Pabasara <mpabasara11@gmail.com>
2021-09-06 01:15:56 +0530

    db upload

---
+++ db backup.sql
@@ -0,0 +1,6 @@
+use food_order
+
+create table client
+(First_Name varchar(20), Last_Name varchar(20), Eamil varchar(30), Contact_no int, Address_line1 varchar(30), Address_line2 varchar(30), Address_line3 varchar(30), Password varchar(20));
+
+select * from client
